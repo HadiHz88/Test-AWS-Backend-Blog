b91de5ca5fa997ce7776420ba1de1cc3c6c83a13
Hadi Hz <hijazih24@gmail.com>
2025-07-07 15:18:59 +0300

    small fix

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ const fs = require("fs");
 const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   //   ssl:
